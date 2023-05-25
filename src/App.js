13a1884aacb7d8a71b5a267db0754abccccdf62e
michal-p99 <michal.pie99@gmail.com>
2023-05-25 15:57:26 +0200

    Add project files to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useEffect, useState} from 'react';
+import SearchIcon from './search-icon.svg';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c74201bc'
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const movies = await fetch(`${API_URL}&s=${title}`);
+    const data = await movies.json();
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('hunger games');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieApp</h1>
+
+      <div className="search">
+        <input
+          placeholder='Search for movies'
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => {}}
+        />
+      </div>
     </div>
   );
 }
